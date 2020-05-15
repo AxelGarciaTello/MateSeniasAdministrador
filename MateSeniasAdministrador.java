08a2abbaf31e4e5141288211bad52cdb7e543bca
AxelGarciaTello <gata21022000@hotmail.com>
2020-05-15 08:39:44 -0500

    Inicio del programa

---
+++ MateSeniasAdministrador.java
@@ -0,0 +1,13 @@
+
+import GUI.IngresoAdministrador;
+
+
+public class MateSeniasAdministrador {
+
+    
+    public static void main(String[] args) {
+        IngresoAdministrador ingreso = new IngresoAdministrador();
+        ingreso.setVisible(true);
+    }
+    
+}
