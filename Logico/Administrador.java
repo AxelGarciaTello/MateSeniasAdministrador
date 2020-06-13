cd7ff523773e82bd88961be84489a6f3866b3d8d
AxelGarciaTello <gata21022000@hotmail.com>
2020-06-13 10:47:46 -0500

    Integración con nuevas funciones

--- Logico/Administrador.java
+++ Logico/Administrador.java
@@ -1,17 +1,36 @@
 
 package Logico;
 
+import Persistencia.ConexionBD;
+import javax.swing.JOptionPane;
+
 
 public class Administrador {
+    private String nombre,
+                   correo,
+                   contrasenia;
     private Tutor[] tutores;
     
-    public Administrador(Tutor[] tutores){
+    public Administrador(String nombre, String correo,
+            String contrasenia, Tutor[] tutores){
+        this.nombre=nombre;
+        this.correo=correo;
+        this.contrasenia=contrasenia;
         this.tutores=tutores;
     }
     
     public void destruir(){
         int tamanio=0,
             x=0;
+        if(nombre!=null){
+            nombre=null;
+        }
+        if(correo!=null){
+            correo=null;
+        }
+        if(contrasenia!=null){
+            contrasenia=null;
+        }
         if(tutores!=null){
             tamanio=tutores.length;
             for(x=0; x<tamanio; x++){
@@ -31,6 +50,10 @@ public class Administrador {
         return tutores;
     }
     
+    public String getCorreo(){
+        return correo;
+    }
+    
     public void setTutor(Tutor nuevo){
         int tamanio=tutores.length,
             x=0;
@@ -46,19 +69,38 @@ public class Administrador {
         int tamanio=tutores.length,
             x=0,
             y=0;
+        boolean aceptacion;
+        String sentencia="DELETE FROM `tutor` WHERE `tutor`.`email` = '"
+                +tutores[posicion].getCorreo()+"';";
         Tutor[] nuevos = new Tutor[tamanio-1];
-        while(x<(tamanio-1)){
-            if(y==posicion){
-                tutores[y]=null;
-                y++;
+        ConexionBD bd = new ConexionBD();
+        aceptacion=bd.administrar(sentencia);
+        sentencia="INSERT INTO `Admin_Tutor` (`idRegistro`, `emailtutor`,"
+                    + " `emailadmin`, `idAccion`) VALUES (NULL, '"
+                    +tutores[posicion].getCorreo()+"', '"+correo+"', '4');";
+        bd.administrar(sentencia);
+        if(aceptacion){
+            while(x<(tamanio-1)){
+                if(y==posicion){
+                    tutores[y]=null;
+                    y++;
+                }
+                else{
+                    nuevos[x]=tutores[y];
+                    x++;
+                    y++;
+                }
             }
-            else{
-                nuevos[x]=tutores[y];
-                x++;
-                y++;
-            }
+            tutores=nuevos;
         }
-        tutores=nuevos;
+        else{
+            JOptionPane.showMessageDialog(
+                    null, "No se pudo eliminar el tutor", "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
     }
+    
+    
     
 }
